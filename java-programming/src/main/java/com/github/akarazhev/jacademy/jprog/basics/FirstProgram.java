adf4bc691885c855f80ef71f58ffbc60d482f38a
Andrey Karazhev <andrey.karazhev@specific-group.com>
2021-08-17 14:01:48 +0300

    Added the first program chapter.
    java programming: basics;

---
+++ java-programming/src/main/java/com/github/akarazhev/jacademy/jprog/basics/FirstProgram.java
@@ -0,0 +1,9 @@
+package com.github.akarazhev.jacademy.jprog.basics;
+
+public final class FirstProgram {
+
+    public static void main(final String[] args) {
+        // Print the word:
+        System.out.println("Hello World!");
+    }
+}
