5015e3a2e9af03b21783190c05235d5a128b794a
Andrey Karazhev <karazhev@gmail.com>
2021-08-24 17:21:34 +0300

    Updated task16.
    java programming: Recursion;

--- practice/src/main/java/com/github/akarazhev/jacademy/practice/jprog/task16/Solution.java
+++ practice/src/main/java/com/github/akarazhev/jacademy/practice/jprog/task16/Solution.java
@@ -1,5 +1,7 @@
 package com.github.akarazhev.jacademy.practice.jprog.task16;
 
+import java.util.Arrays;
+
 public class Solution {
 
     public static void permutations(char[] array, int length) {
@@ -9,7 +11,10 @@ public class Solution {
 
     public static void main(final String[] args) {
         char[] input = {'a', 'b'};
-        System.out.println("Permutations of a string");
+        System.out.println("Permutations of a string: " + Arrays.toString(input));
+        permutations(input, input.length);
+        input = new char[]{'a', 'b', 'b', 'a'};
+        System.out.println("Permutations of a string: " + Arrays.toString(input));
         permutations(input, input.length);
     }
 }
