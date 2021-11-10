0a14534296f9eedc27c2edb68483293ddb8cf93e
Pavel <pashagordievich@gmail.com>
2021-11-10 14:55:14 +0300

    task7

--- practice/src/main/java/com/github/akarazhev/jacademy/practice/jprog/task7/PrintFactors.java
+++ practice/src/main/java/com/github/akarazhev/jacademy/practice/jprog/task7/PrintFactors.java
@@ -1,4 +1,29 @@
 package com.github.akarazhev.jacademy.practice.jprog.task7;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
 public final class PrintFactors {
+
+    public static void main(String[] args) {
+        printFactors(12);
+    }
+
+    public static void printFactors(int number) {
+        int possibleFactor = 1;
+        List<Integer> factors = new LinkedList<>();
+        while (possibleFactor <= number) {
+            if (number % possibleFactor == 0) {
+                System.out.println(possibleFactor + " is a factor of " + number + ".");
+                factors.add(possibleFactor);
+            }
+            possibleFactor++;
+        }
+        //I didn`t understand how should be showing output expression, although I wrote two types.
+        System.out.println("And that's all the factors of " + Arrays.toString(factors.toArray()) + ".");
+        System.out.print("And that's all the factors of ");
+        factors.forEach(x -> System.out.print(x + ", "));
+        System.out.println("\b\b.");
+    }
 }
