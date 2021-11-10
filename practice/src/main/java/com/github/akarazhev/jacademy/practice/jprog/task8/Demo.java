0255f619268ce578fc82e9900b6c097637ccfe4e
Pavel <pashagordievich@gmail.com>
2021-11-10 16:34:40 +0300

    task8

--- practice/src/main/java/com/github/akarazhev/jacademy/practice/jprog/task8/Demo.java
+++ practice/src/main/java/com/github/akarazhev/jacademy/practice/jprog/task8/Demo.java
@@ -2,29 +2,33 @@ package com.github.akarazhev.jacademy.practice.jprog.task8;
 
 final class Calculator {
     // write class fields here
+    private double num1;
+    private double num2;
 
     public Calculator(double num1, double num2) {
         // write definition here
+        this.num1 = num1;
+        this.num2 = num2;
     }
 
     double add() {
         // write definition here
-        return 0;
+        return num1 + num2;
     }
 
     double subtract() {
         // write definition here
-        return 0;
+        return num2 - num1;
     }
 
     double multiply() {
         // write definition here
-        return 0;
+        return num2 * num1;
     }
 
     double divide() {
         // write definition here
-        return 0;
+        return num2/num1;
     }
 }
 
