9417b5d80cacf7238d1f5c137ae0541e4ecdf8e3
Pavel <pashagordievich@gmail.com>
2021-11-10 11:28:57 +0300

    task2

--- practice/src/main/java/com/github/akarazhev/jacademy/practice/jprog/task2/Ged.java
+++ practice/src/main/java/com/github/akarazhev/jacademy/practice/jprog/task2/Ged.java
@@ -1,4 +1,11 @@
 package com.github.akarazhev.jacademy.practice.jprog.task2;
 
 public final class Ged {
+
+    public static void main(String[] args) {
+        String s = "\"Good,\" said the boy, for he had no wish " +
+                "to tell the secret to his playmates, " +
+                "liking to know and do what they knew not and could not.";
+        System.out.println(s);
+    }
 }
