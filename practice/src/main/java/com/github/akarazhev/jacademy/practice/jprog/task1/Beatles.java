2e3d219b4f91d507732e88db5b5ee3ae28275e28
Pavel <pashagordievich@gmail.com>
2021-11-10 11:04:15 +0300

    task1

--- practice/src/main/java/com/github/akarazhev/jacademy/practice/jprog/task1/Beatles.java
+++ practice/src/main/java/com/github/akarazhev/jacademy/practice/jprog/task1/Beatles.java
@@ -1,4 +1,10 @@
 package com.github.akarazhev.jacademy.practice.jprog.task1;
 
 public final class Beatles {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, hello.\n");
+        System.out.println("I don't know why you say goodbye.\n");
+        System.out.println("I say hello.");
+    }
 }
