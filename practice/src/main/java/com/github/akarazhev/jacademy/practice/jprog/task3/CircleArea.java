f61e34f767ea7a433a922372412d0f28905ece66
Pavel <pashagordievich@gmail.com>
2021-11-10 11:42:55 +0300

    task3

--- practice/src/main/java/com/github/akarazhev/jacademy/practice/jprog/task3/CircleArea.java
+++ practice/src/main/java/com/github/akarazhev/jacademy/practice/jprog/task3/CircleArea.java
@@ -1,4 +1,10 @@
 package com.github.akarazhev.jacademy.practice.jprog.task3;
 
 public final class CircleArea {
+
+    public static void main(String[] args) {
+        double r = 2, pi = Math.PI, areaOfTheCircle;
+        areaOfTheCircle = r * r * pi;
+        System.out.println("Area of the circle:" + areaOfTheCircle);
+    }
 }
