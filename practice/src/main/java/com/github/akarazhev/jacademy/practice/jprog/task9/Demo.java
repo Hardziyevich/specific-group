d7d131b67611d01aaeea070271dbf245be7382d9
Pavel <pashagordievich@gmail.com>
2021-11-11 10:08:57 +0300

    task9

--- practice/src/main/java/com/github/akarazhev/jacademy/practice/jprog/task9/Demo.java
+++ practice/src/main/java/com/github/akarazhev/jacademy/practice/jprog/task9/Demo.java
@@ -3,23 +3,31 @@ package com.github.akarazhev.jacademy.practice.jprog.task9;
 // Rectangle Class
 final class Rectangle {
     // Write Fields Here
+    private int length;
+    private int width;
 
     public Rectangle() {
         // Write your code here
+        this.length = 0;
+        this.width = 0;
     }
 
     public Rectangle(int length, int width) {
         // Write your code here
+        this.length = length;
+        this.width = width;
     }
 
     public int getArea() {
         // Write your code here
-        return 0;
+        return length * width;
     }
 }
 
 public final class Demo {
 
     public static void main(final String[] args) {
+        Rectangle obj = new Rectangle(2,2);
+        System.out.println(obj.getArea());
     }
 }
