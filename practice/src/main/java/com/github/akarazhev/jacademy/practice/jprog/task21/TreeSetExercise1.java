6abf31e4c12f89c0093ac6abdd3c2590de3d64d0
Pavel <pashagordievich@gmail.com>
2021-11-16 12:02:28 +0300

    task21

--- practice/src/main/java/com/github/akarazhev/jacademy/practice/jprog/task21/TreeSetExercise1.java
+++ practice/src/main/java/com/github/akarazhev/jacademy/practice/jprog/task21/TreeSetExercise1.java
@@ -1,9 +1,17 @@
 package com.github.akarazhev.jacademy.practice.jprog.task21;
 
+import java.util.*;
+
 public final class TreeSetExercise1 {
 
     public static void main(final String[] args) {
         int[] numbers = {1, 4, 5, 2, 34, 66, 5, 4, 33, 45, 6, 8, 56, 76, 78, 98, 34, 37, 12, 12, 23, 43, 54, 56};
         // Your code goes here
+        Arrays.stream(findAllDigitGreaterThan(numbers, 50)).forEach(System.out::println);
+    }
+
+    //Method is working and finding digits in array which greater than input digit
+    private static int[] findAllDigitGreaterThan(int[] number, int digit){
+        return Arrays.stream(number).filter(x -> x > digit).toArray();
     }
 }
--- practice/src/main/java/com/github/akarazhev/jacademy/practice/jprog/task21/TreeSetExercise2.java
+++ practice/src/main/java/com/github/akarazhev/jacademy/practice/jprog/task21/TreeSetExercise2.java
@@ -1,9 +1,15 @@
 package com.github.akarazhev.jacademy.practice.jprog.task21;
 
+import java.util.Arrays;
+
 public final class TreeSetExercise2 {
 
     public static void main(final String[] args) {
         int[] numbers = {1, 4, 5, 2, 34, 66, 5, 4, 33, 45, 6, 8, 56, 76, 78, 98, 34, 37, 12, 12, 23, 43, 54, 56};
         // Your code goes here
+        //finding min and max
+        int min = Arrays.stream(numbers).min().getAsInt();
+        int max = Arrays.stream(numbers).max().getAsInt();
+        System.out.printf("Min:%d\nMax:%d%n",min,max);
     }
 }
