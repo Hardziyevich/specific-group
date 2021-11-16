47271ab1c144f03dab6b97f82c8336e5c4fb3e4a
Andrey Karazhev <karazhev@gmail.com>
2021-11-16 14:08:47 +0300

    Added codes for the Multithreading chapter.

---
+++ java-programming/src/main/java/com/github/akarazhev/jacademy/jprog/multithreading/synch/v1/BadSynchronization.java
@@ -0,0 +1,12 @@
+package com.github.akarazhev.jacademy.jprog.multithreading.synch.v1;
+
+public class BadSynchronization {
+
+    public static void main(String args[]) throws InterruptedException {
+        Object dummyObject = new Object();
+
+        // Attempting to call wait() on the object
+        // outside of a synchronized block.
+        dummyObject.wait();
+    }
+}
---
+++ java-programming/src/main/java/com/github/akarazhev/jacademy/jprog/multithreading/synch/v2/BadSynchronization.java
@@ -0,0 +1,17 @@
+package com.github.akarazhev.jacademy.jprog.multithreading.synch.v2;
+
+public class BadSynchronization {
+
+    public static void main(String args[]) {
+        Object dummyObject = new Object();
+        Object lock = new Object();
+
+        synchronized (lock) {
+            lock.notify();
+
+            // Attempting to call notify() on the object
+            // in synchronized block of another object
+            dummyObject.notify();
+        }
+    }
+}
