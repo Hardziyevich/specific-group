3d957b4ba8c909b5fe215eeabc68dcbe287eeeb7
Pavel <pashagordievich@gmail.com>
2021-11-16 10:26:03 +0300

    task21

--- practice/src/main/java/com/github/akarazhev/jacademy/practice/jprog/task20/HashSetExercise.java
+++ practice/src/main/java/com/github/akarazhev/jacademy/practice/jprog/task20/HashSetExercise.java
@@ -1,14 +1,32 @@
 package com.github.akarazhev.jacademy.practice.jprog.task20;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public final class HashSetExercise {
 
     public static void main(final String[] args) {
         int[] data = {12, 34, 1, 56, 43, 34, 65};
-        int duplicate = 0;
+        int duplicate = findDuplicateElement(data);
 
         // Your logic will go here
 
         System.out.println(duplicate);
+    }
+
+    //Method is working and finding duplicate element in array.
+    public static int findDuplicateElement(int[] array) {
+        Set<Integer> temp = new HashSet<>();
+        int result = 0;
+        //traversing through array and put all elements in HashSet
+        for (Integer integer : array) {
+            if (!temp.add(integer)) {
+                result = integer;
+                break;
+            }
+        }
+        return result;
     }
 }
 
