fa67147b7548947307c1bb8230de78de0b4fac9a
Andrey Karazhev <karazhev@gmail.com>
2021-11-16 13:11:08 +0300

    Added codes for the OOP chapter.

---
+++ java-programming/src/main/java/com/github/akarazhev/jacademy/jprog/oop/aggregation/Main.java
@@ -0,0 +1,43 @@
+package com.github.akarazhev.jacademy.jprog.oop.aggregation;
+
+class Country {
+    private String name;
+    private int population;
+
+    public Country(String n, int p) {
+        name = n;
+        population = p;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+class Person {
+    private String name;
+    private Country country; // An instance of Country class
+
+    public Person(String n, Country c) {
+        name = n;
+        country = c;
+    }
+
+    public void printDetails() {
+        System.out.println("Name: " + name);
+        System.out.println("Country: " + country.getName());
+    }
+}
+
+public class Main {
+
+    public static void main(String args[]) {
+        Country country = new Country("Utopia", 1);
+        {
+            Person user = new Person("Darth Vader", country);
+            user.printDetails();
+        }
+        // The user object's lifetime is over
+        System.out.println(country.getName()); // The country object still exists!
+    }
+}
