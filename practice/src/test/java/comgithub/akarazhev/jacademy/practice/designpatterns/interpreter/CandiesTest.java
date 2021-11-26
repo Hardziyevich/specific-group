307b29eef35031faa53aaa1d455701d3109b2e38
Pavel <pashagordievich@gmail.com>
2021-11-26 13:03:13 +0300

    Added interpretter, which provides an opportunity to recognize type of candy and their volume

---
+++ practice/src/test/java/comgithub/akarazhev/jacademy/practice/designpatterns/interpreter/CandiesTest.java
@@ -0,0 +1,21 @@
+package comgithub.akarazhev.jacademy.practice.designpatterns.interpreter;
+
+import com.github.akarazhev.jacademy.practice.designpatterns.interpreter.InterpreterCandies;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CandiesTest {
+
+    @Test
+    void testCandies(){
+
+        InterpreterCandies interType = InterpreterCandies.typeCandy(InterpreterCandies.TypeCandies.CARAMEL);
+        InterpreterCandies value = InterpreterCandies.valueCandy(InterpreterCandies.ValueCandies.FAT);
+        assertAll(()->{
+            assertTrue(interType.interpret("caramel"));
+            assertTrue(InterpreterCandies.isCandies("caramel"));
+            assertTrue(value.interpret("fat"));
+        });
+    }
+}
