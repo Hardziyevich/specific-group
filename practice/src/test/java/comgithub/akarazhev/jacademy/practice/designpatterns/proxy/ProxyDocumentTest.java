88f083d25334d446adcdf41e1e163a239f98c5e7
Pavel <pashagordievich@gmail.com>
2021-11-26 10:22:35 +0300

    Changed test for proxe. Added search file from folder resources

--- practice/src/test/java/comgithub/akarazhev/jacademy/practice/designpatterns/proxy/ProxyDocumentTest.java
+++ practice/src/test/java/comgithub/akarazhev/jacademy/practice/designpatterns/proxy/ProxyDocumentTest.java
@@ -5,25 +5,28 @@ import com.github.akarazhev.jacademy.practice.designpatterns.proxy.DocumentTxt;
 import com.github.akarazhev.jacademy.practice.designpatterns.proxy.ProxyDocumentTxt;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.nio.file.Path;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProxyDocumentTest {
-    Document rightPath = new ProxyDocumentTxt(Path.of("C:\\work\\byMyMentor\\jacademy-base\\practice\\src\\main\\resources\\1.txt"));
-
+    private final URL is = getClass().getClassLoader().getResource("1.txt");
+    private final File file = new File(is.getFile());
+    Document rightPath = new ProxyDocumentTxt(Path.of(file.getPath()));
     public ProxyDocumentTest() throws IOException {
     }
 
     @Test
     void testAllMethode() {
-        Document document = new DocumentTxt(Path.of("C:\\work\\byMyMentor\\jacademy-base\\practice\\src\\main\\resources\\1.txt"));
+        Document document = new DocumentTxt(Path.of(file.getPath()));
         document.isDownload();
         assertAll(() -> {
             assertTrue(rightPath.isDownload());
             assertEquals(document.getInfo(), rightPath.getInfo());
-            assertThrows(IOException.class, () -> new ProxyDocumentTxt(Path.of("C:\\work\\byMyMentor\\jacademy-base\\practice\\src\\main\\resources\\1.t")));
+            assertThrows(IOException.class, () -> new ProxyDocumentTxt(Path.of("1.t")));
         });
     }
 }
